f5f01ca323c13108d92ead0a2855ea5d9a22f76c
orkanmiguel <orkan.miguel@gmail.com>
2024-10-30 00:34:20 -0300

    output hybrid

--- src/pages/api/get-person.json.ts
+++ src/pages/api/get-person.json.ts
@@ -1,5 +1,15 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ params, request }) => {
-  return new Response("OK!", { status: 200 });
+  const person = {
+    name: "Orkan Miguel",
+    rol: "dev",
+  };
+
+  return new Response(JSON.stringify(person), {
+    status: 200,
+    headers: {
+      "Contetn-Type": "aplication/json",
+    },
+  });
 };
